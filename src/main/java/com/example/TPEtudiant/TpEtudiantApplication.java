8ba488a8896da7751b7205e488614d71ee72248f
VORONIN Roman <voronin.roman.i@gmail.com>
2021-08-06 18:14:43 +0200

    initial commit

---
+++ src/main/java/com/example/TPEtudiant/TpEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPEtudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEtudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TPEtudiant/TpEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TPEtudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
